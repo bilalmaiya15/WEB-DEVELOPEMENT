208c8e97f12290386eb61bc0b731a1aadc177a9e
bilalmaiya15 <bilalmaiya15@gmail.com>
2025-10-24 23:37:29 -0700

    Add files via upload

---
+++ java game/app.js
@@ -0,0 +1,13 @@
+let num = prompt("enter your numebr")
+if (num == 4){
+    console.log("correct")
+}
+ else if (num == 3){
+    console.log("QareebHo")
+}
+ if (num == 5){
+    console.log("Aik Step")
+}
+else if (num == 10){
+    console.log("Try Again")
+}
